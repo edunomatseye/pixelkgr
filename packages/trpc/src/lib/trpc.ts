8c0e8b24e11b1fb3d95b2f9930c2db3889a05d9f
Edun Omatseye <edunomatseye@yahoo.com>
2023-08-24 14:07:21 +0100

    chore: tRPC library initialization

--- packages/trpc/src/lib/trpc.ts
+++ packages/trpc/src/lib/trpc.ts
@@ -1,5 +1,22 @@
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import superjson from 'superjson';
+import { createContext } from './context';
+
+type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+const router = t.router;
+const procedure = t.procedure;
+const middleware = t.middleware;
+const mergeRouters = t.mergeRouters;
+
+export type { Context };
+export { t, router, procedure, middleware, mergeRouters };
 export function trpc(): string {
   return 'trpc';
 }
-
-import { inferAsyncReturnType, initTRPC } from '@trpc/server';
